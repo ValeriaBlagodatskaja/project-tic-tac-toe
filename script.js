70863d2662802146cee7ec2599b0259396dc1ef9
Valeria48562 <vblagodatskaja@gmail.com>
2023-09-01 08:21:13 +0300

    commit

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const cellRef = document.querySelectorAll(".cell");
 const resultSpan = document.getElementById("result");
 const restartButton = document.getElementById("restartButton");
+const gameBoard = document.getElementById("gameBoard");
+const newGameForm = document.getElementById("newGameForm");
+const playerXName = document.getElementById("playerXName");
+const playerOName = document.getElementById("playerOName");
+const startButton = document.getElementById("startButton");
+const twoPlayersButton = document.querySelector(".twoPlayersButton");
+
 let xTurn = true;
+let gameWon = false;
+let playerNameX = "Player X";
+let playerNameO = "Player O";
+
+twoPlayersButton.addEventListener("click", () => {
+  newGameForm.style.display = "block";
+});
+
+startButton.addEventListener("click", function startGame(event) {
+  event.preventDefault();
+  const nameX = playerX.value || "Player X";
+  const nameO = playerO.value || "Player O";
+  playerXName.textContent = nameX;
+  playerOName.textContent = nameO;
+  newGameForm.style.display = "none";
+  gameBoard.style.display = "flex";
+  enableButtons();
+});
 
 function checkWinner() {
   const winningCombinations = [
@@ -21,6 +46,7 @@ function checkWinner() {
       cellRef[a].textContent === cellRef[b].textContent &&
       cellRef[a].textContent === cellRef[c].textContent
     ) {
+      gameWon = true;
       return cellRef[a].textContent;
     }
   }
@@ -33,21 +59,28 @@ function checkWinner() {
 //Display X/0 on click
 cellRef.forEach((element) => {
   element.addEventListener("click", () => {
-    if (xTurn) {
-      xTurn = false;
-      element.innerText = "X";
-      element.disabled = true;
-    } else {
-      xTurn = true;
-      element.innerText = "O";
-      element.disabled = true;
-    }
-    const winner = checkWinner();
-    if (winner) {
-      if (winner === "draw") {
-        resultSpan.textContent = "It's a draw!";
+    if (!element.textContent && !gameWon) {
+      if (xTurn) {
+        xTurn = false;
+        element.innerText = "X";
+        element.disabled = true;
       } else {
-        resultSpan.textContent = `${winner} wins!`;
+        xTurn = true;
+        element.innerText = "O";
+        element.disabled = true;
+      }
+      const winner = checkWinner();
+      if (winner) {
+        if (winner === "draw") {
+          resultSpan.textContent = "It's a draw!";
+        } else {
+          const winnerName = winner === "X" ? playerNameX : playerNameO;
+          resultSpan.textContent = `${winnerName} wins!`;
+        }
+        cellRef.forEach((cell) => {
+          cell.disabled = true;
+        });
+        gameWon = true;
       }
     }
   });
